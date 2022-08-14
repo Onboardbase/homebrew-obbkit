726adda5324bb9e24857c67e1227e9ed5ee71a05
AdeboyeDN <adeboyed93@gmail.com>
2022-08-14 22:58:16 +0100

    Brew formula update for obbkit version v0.5.0

--- obbkit.rb
+++ obbkit.rb
@@ -5,11 +5,11 @@
 class Obbkit < Formula
   desc ""
   homepage "https://github.com/Onboardbase/obbkit"
-  version "0.4.0"
+  version "0.5.0"
 
   on_macos do
-    url "https://github.com/Onboardbase/obbkit/releases/download/v0.4.0/obbkit_0.4.0_Darwin_all.tar.gz"
-    sha256 "27fecaaa5085e3b561f196a0af0ae9f1bd4e91bd2eecf7b19a36f409a7b88385"
+    url "https://github.com/Onboardbase/obbkit/releases/download/v0.5.0/obbkit_0.5.0_Darwin_all.tar.gz"
+    sha256 "426b131ba6e571716f0d7b31d79348da2a7e6764e54cd3977f14797237764043"
 
     def install
       bin.install "obbkit"
@@ -17,17 +17,17 @@ class Obbkit < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/Onboardbase/obbkit/releases/download/v0.4.0/obbkit_0.4.0_Linux_x86_64.tar.gz"
-      sha256 "ef657db06e99646655e42cd26dbd7cb922ac2ae05c88283f01cfb62033c4a6da"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/Onboardbase/obbkit/releases/download/v0.5.0/obbkit_0.5.0_Linux_arm64.tar.gz"
+      sha256 "f4ee4e0b9b8116b9d82cf4730c7feb05d6c108966b93903fe0a3baf7d9bde067"
 
       def install
         bin.install "obbkit"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Onboardbase/obbkit/releases/download/v0.4.0/obbkit_0.4.0_Linux_arm64.tar.gz"
-      sha256 "89dddf5d1c91d821dc946dfadebc642c9b50dd95af9f6c6e98c98ed97e174112"
+    if Hardware::CPU.intel?
+      url "https://github.com/Onboardbase/obbkit/releases/download/v0.5.0/obbkit_0.5.0_Linux_x86_64.tar.gz"
+      sha256 "8b79f423dc35556a612d8680d699c97165db1d8002ad5ef256f752259139b6a6"
 
       def install
         bin.install "obbkit"
